6bc350ad2b213c06ada326560c38aaa06a20bea6
Matteus <matteussantos30@gmail.com>
2025-11-11 20:42:03 -0300

    feat: Adicionado integracao do Application Insights da Azure

--- src/main/java/com/fiap/feedbacksystem/FeedbacksystemApplication.java
+++ src/main/java/com/fiap/feedbacksystem/FeedbacksystemApplication.java
@@ -1,5 +1,6 @@
 package com.fiap.feedbacksystem;
 
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FeedbacksystemApplication {
 
 	public static void main(String[] args) {
+		// Ativa o Azure Application Insights por código.
+		// Precisa vir antes do SpringApplication.run().
+		ApplicationInsights.attach();
+		
 		SpringApplication.run(FeedbacksystemApplication.class, args);
 	}
 
